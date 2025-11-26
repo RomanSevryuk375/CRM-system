059e4d5e89729e1e0e4ba265c3cf876f9ec939ae
RomanSevryuk375 <sevryuk.roma@mail.ru>
2025-11-26 22:47:00 +0300

    replace commit

--- CRM-system/src/api/statuses.js
+++
@@ -1,9 +0,0 @@
-import { makeRequest } from "./makeRequest";
-
-const URL = "/Status";
-
-export const getStatuses = (config) => makeRequest({
-    method: "GET",
-    url: URL,
-    ...config,
-});
