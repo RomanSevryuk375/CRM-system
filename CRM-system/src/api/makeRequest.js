3007a406ae64dc988bd8e2767b1d4fb6ad01a2f9
RomanSevryuk375 <sevryuk.roma@mail.ru>
2025-11-26 22:40:55 +0300

    replace commit

--- CRM-system/src/api/makeRequest.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_ENDPOINT = "http://localhost:5066";
-
-export const makeRequest = (config) => {
-    config.url = `${API_ENDPOINT}${config.url}`;
-    
-    return axios(config);
-};
--- CRM-system/src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import { thunk } from "redux-thunk";
-import { rootReducer } from "./reducers/rootReducer.js";
-
-export const store = createStore(rootReducer, applyMiddleware(thunk));
