d631bde1a6e88365d9df61f7ddebbf9e8c935c8e
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-02-07 13:48:28 +0300

    feat(mobile/service) implement work service

---
+++ Mobile/CRMSystemMobile/Services/WorkService.cs
@@ -0,0 +1,103 @@
+﻿using Shared.Contracts.Work;
+using Shared.Filters;
+using System.Diagnostics;
+using System.Net.Http.Json;
+
+namespace CRMSystemMobile.Services;
+
+public class WorkService(HttpClient httpClient)
+{
+    public async Task<(List<WorkResponse>?, int TotalCount)> GetWorks(WorkFilter filter)
+    {
+        try
+        {
+            var query = $"Page={filter.Page}&Limit={filter.Limit}&IsDescending={filter.IsDescending}";
+
+            if (!string.IsNullOrEmpty(filter.SortBy))
+            {
+                query += $"&SortBy={filter.SortBy}";
+            }
+
+            string url = $"api/Work?{query}";
+
+            var response = await httpClient.GetAsync(url);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                SecureStorage.Default.Remove("jwt_token");
+                await Shell.Current.GoToAsync("//LoginPage");
+                return (null, 0);
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            int totalCount = 0;
+            if (response.Headers.TryGetValues("x-total-count", out var values))
+            {
+                int.TryParse(values.FirstOrDefault(), out totalCount);
+            }
+
+            var items = await response.Content.ReadFromJsonAsync<List<WorkResponse>>();
+            return (items, totalCount);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.ToString());
+            return (null, 0);
+        }
+    }
+
+    public async Task<string?> CreateWork(WorkRequest request)
+    {
+        try
+        {
+            var response = await httpClient.PostAsJsonAsync($"api/Work", request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var errorContent = await response.Content.ReadAsStringAsync();
+
+            if (string.IsNullOrWhiteSpace(errorContent))
+            {
+                return $"Ошибка сервера: {response.StatusCode}";
+            }
+
+            return errorContent.Trim('"');
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.ToString()); 
+            return null;
+        }
+    }
+
+    public async Task<string?> DeleteWork(long id)
+    {
+        try
+        {
+            var response = await httpClient.DeleteAsync($"api/Work{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var errorContent = await response.Content.ReadAsStringAsync();
+
+            if (string.IsNullOrWhiteSpace(errorContent))
+            {
+                return $"Ошибка сервера: {response.StatusCode}";
+            }
+
+            return errorContent.Trim('"');
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.ToString());
+            return null;
+        }
+    }
+}
