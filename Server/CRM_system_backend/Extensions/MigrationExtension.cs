a5cdb3db450c6d61b0d87a0cc39c4bc5774dc199
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-02-22 18:27:27 +0300

    refactor(server/api) implement extension methods for program.cs

---
+++ Server/CRM_system_backend/Extensions/MigrationExtension.cs
@@ -0,0 +1,14 @@
+﻿using CRMSystem.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM_system_backend.Extensions;
+
+public static class MigrationExtension
+{
+    public static void ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<SystemDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
